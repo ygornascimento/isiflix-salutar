91da73f6f36a4beb0e3dba7b18f7cfb33abb20b0
Ygor Nascimento <ygor.nascimento@outlook.com>
2024-07-18 19:23:28 -0300

    Start of the Salutar Project and database configuration

---
+++ salutar/src/main/java/br/com/isiflix/salutar/SalutarApplication.java
@@ -0,0 +1,13 @@
+package br.com.isiflix.salutar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalutarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalutarApplication.class, args);
+	}
+
+}
