e4170125264f52d7d52492deb80f0563bf32f563
Ygor Nascimento <ygor.nascimento@outlook.com>
2024-08-04 18:27:11 -0300

    ERROR - Field id_usuario doesnt have a default value

--- salutar/src/main/java/br/com/isiflix/salutar/security/WebSecurityConfig.java
+++ salutar/src/main/java/br/com/isiflix/salutar/security/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -14,14 +15,24 @@ public class WebSecurityConfig {
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-			.authorizeHttpRequests()
-			.requestMatchers(HttpMethod.POST, "/usuarios").permitAll()
-			.requestMatchers(HttpMethod.POST, "/login").permitAll()
-			.anyRequest().authenticated().and().cors();
+//		http.csrf(csrf -> csrf.disable())
+//			.authorizeHttpRequests(auth -> auth
+//					.requestMatchers(HttpMethod.POST, "/usuarios").permitAll()
+//					.requestMatchers(HttpMethod.POST, "/login").permitAll()
+//					.anyRequest().authenticated())
+//			.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//					.httpBasic(Customizer.withDefaults())
+//					.build();
+//		http.addFilterBefore(new MyFilter(), UsernamePasswordAuthenticationFilter.class);
+//		
+//		return http.build();
 		
-		http.addFilterBefore(new MyFilter(), UsernamePasswordAuthenticationFilter.class);
-		
-		return http.build();
+        return http.csrf(csrf -> csrf.disable())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize.requestMatchers(HttpMethod.POST, "/login").permitAll()
+                .requestMatchers(HttpMethod.POST, "/usuarios").permitAll()
+                .anyRequest().authenticated())
+                .addFilterBefore(new MyFilter(), UsernamePasswordAuthenticationFilter.class)
+                .build();
 	}
 }
